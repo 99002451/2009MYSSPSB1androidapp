6351550fe7080455513d6350fc5e8c86e6bf0c81
99002451 <user_name>
2020-11-04 22:34:18 -0800

    MainActivity created - Santwana

--- app/src/main/java/com/example/a2009mysspsb1androidapp/MainActivity.java
+++ app/src/main/java/com/example/a2009mysspsb1androidapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.a2009mysspsb1androidapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button joinNowButton, loginButton;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton = findViewById(R.id.main_join_now_btn);
+        loginButton = findViewById(R.id.main_login_btn);
+        loadingBar = new ProgressDialog(this);
+
+       // Paper.init(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
 }
