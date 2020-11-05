78687f1a935af65a5ee14fe71b4a9acd5d7f61da
99002600 <rahul.rauniyar@ltts.com>
2020-11-05 14:55:14 +0530

    Third Commit Of Register

--- app/src/main/java/com/example/a2009mysspsb1androidapp/RegisterActivity.java
+++ app/src/main/java/com/example/a2009mysspsb1androidapp/RegisterActivity.java
@@ -1,67 +1,20 @@
-private void CreateAccout()
-        {
-        String name = InputName.getText().toString();
-        String phone = InputPhoneNumber.getText().toString();
-        String password = InputPassword.getText().toString();
-
-        if (TextUtils.isEmpty(name))
-        {
-        Toast.makeText(this, "Please Write Your Name...", Toast.LENGTH_SHORT).show();
-        }
-        else if (TextUtils.isEmpty(phone))
-        {
-        Toast.makeText(this, "Please Write Your Phone Number...", Toast.LENGTH_SHORT).show();
-        }
-        else if (TextUtils.isEmpty(password))
-        {
-        Toast.makeText(this, "Please Write Your Password...", Toast.LENGTH_SHORT).show();
-        }
+}
         else
         {
-        loadingBar.setTitle("Create Account");
-        loadingBar.setMessage("Please wait while we are checking the details");
-        loadingBar.setCanceledOnTouchOutside(false);
-        loadingBar.show();
-
-        ValidatephoneNumber(name, phone, password);
-        }
-        }
-
-private void ValidatephoneNumber(final String name, final String phone, final String password)
-        {
-final DatabaseReference RootRef;
-
-        RootRef = FirebaseDatabase.getInstance().getReference();
-
-        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
-@Override
-public void onDataChange(@NonNull DataSnapshot dataSnapshot)
-        {
-        if (!(dataSnapshot.child("Users").child(phone).exists()))
-        {
-        HashMap<String, Object> userdataMap = new HashMap<>();
-        userdataMap.put("phone", phone);
-        userdataMap.put("password", password);
-        userdataMap.put("name", name);
-
-        RootRef.child("Users").child(phone).updateChildren(userdataMap)
-        .addOnCompleteListener(new OnCompleteListener<Void>() {
-@Override
-public void onComplete(@NonNull Task<Void> task)
-        {
-        if (task.isSuccessful())
-        {
-        Toast.makeText(RegisterActivity.this, "Congratulations, your account has been created", Toast.LENGTH_SHORT).show();
+        Toast.makeText(RegisterActivity.this, "This " + phone + " Already exists...", Toast.LENGTH_SHORT).show();
         loadingBar.dismiss();
 
-        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        Toast.makeText(RegisterActivity.this, "Please try using another phone number", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
         startActivity(intent);
         }
-        else
-        {
-        loadingBar.dismiss();
-        Toast.makeText(RegisterActivity.this, "Network Error: Please try again after sometime...", Toast.LENGTH_SHORT).show();
         }
+
+@Override
+public void onCancelled(@NonNull DatabaseError databaseError) {
+
         }
         });
-           
+        }
+        }
