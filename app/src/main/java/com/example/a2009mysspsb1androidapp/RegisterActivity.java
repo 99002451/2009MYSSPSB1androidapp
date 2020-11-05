1cc8fe251205a01749906f5fd86cd320699b40b1
99002600 <rahul.rauniyar@ltts.com>
2020-11-05 12:45:09 +0530

    First Commit Of Register

--- app/src/main/java/com/example/a2009mysspsb1androidapp/RegisterActivity.java
+++ app/src/main/java/com/example/a2009mysspsb1androidapp/RegisterActivity.java
@@ -1,4 +1,32 @@
-package com.example.a2009mysspsb1androidapp;
+package com.example.a2009mysspsb1;
 
-public class RegisterActivity {
-}
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity
+{
+    private Button CreateAccountButton;
+    private EditText InputName, InputPhoneNumber, InputPassword;
+    private ProgressDialog loadingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        CreateAccountButton = findViewById(R.id.register_btn);
+        InputName = findViewById(R.id.register_username_input);
+        InputPhoneNumber = findViewById(R.id.register_phone_number_input);
+        InputPassword = findViewById(R.id.register_password_input);
+        loadingBar = new ProgressDialog(this);
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                CreateAccout();
+
+            }
+        });
+    }
