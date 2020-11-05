d359e04db79902a779fccc206f8c4fdd399a30f8
99002600 <rahul.rauniyar@ltts.com>
2020-11-05 22:00:28 +0530

    Third Commit Of Register

--- app/src/main/java/com/example/a2009mysspsb1androidapp/LoginActivity.java
+++ app/src/main/java/com/example/a2009mysspsb1androidapp/LoginActivity.java
@@ -1,4 +1,59 @@
 package com.example.a2009mysspsb1androidapp;
 
-public class LoginActivity {
-}
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ecommerce.Model.Users;
+import com.example.ecommerce.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rey.material.widget.CheckBox;
+
+import io.paperdb.Paper;
+
+
+public class LoginActivity extends AppCompatActivity
+{
+    private EditText InputPhoneNumber, InputPassword;
+    private Button LoginButton;
+    private ProgressDialog loadingBar;
+    private TextView AdminLink, NotAdminLink;
+
+    private String parentDbName = "Users";
+    private CheckBox chkBoxRememberMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        LoginButton = findViewById(R.id.login_btn);
+        InputPassword = findViewById(R.id.login_password_input);
+        InputPhoneNumber = findViewById(R.id.login_phone_number_input);
+        AdminLink = findViewById(R.id.admin_panel_link);
+        NotAdminLink = findViewById(R.id.not_admin_panel_link);
+        loadingBar = new ProgressDialog(this);
+
+        chkBoxRememberMe = findViewById(R.id.remember_me_chkb);
+        Paper.init(this);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                LoginUser();
+
+            }
+        });
